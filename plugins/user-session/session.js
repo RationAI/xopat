0eb77c06a2a3038adfd207a4c5e154f62f5476fd
Aiosa <horakj7@gmail.com>
2023-09-18 16:48:34 +0200

    Move app config serialization to just one function. Add session ability to export without auth.

--- plugins/user-session/session.js
+++ plugins/user-session/session.js
@@ -9,10 +9,11 @@ addPlugin("user-session", class extends XOpatPlugin {
     }
 
     pluginReady() {
-        this.authenticate();
-
         //todo this code is duplicated
         this.setupActiveTissue(APPLICATION_CONTEXT.config.background[APPLICATION_CONTEXT.getOption('activeBackgroundIndex', 0)]);
+
+        this.authenticate();
+
         const _this = this;
         VIEWER.addHandler('background-image-swap', e => {
             _this.setupActiveTissue(e.backgroundSetup);
@@ -25,7 +26,10 @@ addPlugin("user-session", class extends XOpatPlugin {
     }
 
     authenticate(repeatedLogin=true) {
-        if (!this.authServer || this.authenticated) return; //todo message
+        if (!this.authServer || this.authenticated) {
+            this._finishNoAuth();
+            return; //todo message
+        }
 
         const _this = this;
         UTILITIES.fetchJSON(this.authServer, {}, this.headers).then(response => {
@@ -74,6 +78,20 @@ addPlugin("user-session", class extends XOpatPlugin {
         }
     }
 
+    _finishNoAuth() {
+        if (this.activeTissue && this.storeSessionServer) {
+            USER_INTERFACE.MainMenu.replace(
+                `Session store`,
+                `<span class="btn-pointer" title="Store your workplace on the server." 
+style="text-align:right; vertical-align:sub;float: right;" onclick="${this.THIS}.export();">
+Save Session: <span class="material-icons">save</span></span>`,
+                '',
+                "user-session-panel",
+                this.id
+            );
+        }
+    }
+
     _finishAuthOk(response) {
         const meta = APPLICATION_CONTEXT.metadata;
         meta.set(xOpatSchema.user, response);
@@ -101,17 +119,34 @@ style="text-align:right; text-decoration: line-through; vertical-align:sub;float
         );
     }
 
+    async viewerToHTML() {
+        return `<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head><meta charset="utf-8"><title>Visualisation export</title></head>
+<body>
+${await UTILITIES.getForm()}
+</body></html>`;
+    }
+
     async export() {
         if (!this.storeSessionServer) {
             console.warn("Cannot save the session: no target WSI found.");
             Dialogs.show("Cannot save the session: no target WSI found.", 2500, Dialogs.MSG_WARN);
         } else {
+            const exporter = this.getStaticMeta("exportHTML") ? this.viewerToHTML : UTILITIES.serializeApp;
+
             UTILITIES.fetchJSON(this.storeSessionServer, {
-                data: await UTILITIES.serializeApp(),
+                data: await exporter(),
                 tissue: this.activeTissue
             }, this.headers).then(response => {
                 if (response?.status !== "success") throw new HTTPError(response.message, response, response.error);
-                Dialogs.show("Saved", 1500, Dialogs.MSG_INFO);
+
+                if (response.url) {
+                    UTILITIES.copyToClipboard(response.url);
+                    Dialogs.show("Url saved in your clipboard.", 1500, Dialogs.MSG_INFO);
+                } else {
+                    Dialogs.show("Saved", 1500, Dialogs.MSG_INFO);
+                }
             }).catch(e => {
                 console.warn("Failed to save export to server.", e);
                 Dialogs.show("Failed to save the session!", 2500, Dialogs.MSG_ERR);
