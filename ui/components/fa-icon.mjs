1b0469df520d1b22a3dae851842c4746ab8cfe88
Jiří Buchta <jiribuchta02@gmail.com>
2024-12-12 12:11:09 +0100

    adding new ui buttons to xopat

---
+++ ui/components/fa-icon.mjs
@@ -0,0 +1,24 @@
+import van from "../vanjs.mjs";
+import { BaseComponent } from "./baseComponent.mjs";
+
+const { i } = van.tags
+
+class FAIcon extends BaseComponent {
+    constructor(options, ...args) {
+        super(options, ...args);
+
+        this.classMap["base"] = "fa-solid";
+        this.classMap["name"] = "";
+
+        if (options) {
+            if (options.name) this.classMap["name"] = options.name;
+        }
+    }
+    create() {
+        return i(
+            { ...this.commonProperties},
+        )
+    }
+}
+
+export { FAIcon };
--- ui/index.mjs
+++ ui/index.mjs
@@ -1,9 +1,78 @@
 globalThis.UI = {};
+globalThis.VANCOMPONENTS = {};
 
+import van from "./vanjs.mjs";
 import { Hello } from "./test.mjs"; // TODO
 import { PrimaryButton } from "./components/buttons.mjs"; // TODO
 import { Collapse } from "./components/collapse.mjs"; // TODO
-const UI = { Hello, PrimaryButton, Collapse };
+import { FAIcon } from "./components/fa-icon.mjs";
+const UI = { Hello, PrimaryButton, Collapse, FAIcon };
 globalThis.UI = UI;
 export default UI;
 //console.log(globalThis);
+
+// globalThis.vanInject = function (id, thiss) {
+//     const component = globalThis.VANCOMPONENTS[id];
+//     thiss.appendChild(component);
+// }
+
+// globalThis.vanReplace = function (id, thiss) {
+//     console.log("vanReplace");
+//     const component = globalThis.VANCOMPONENTS[id];
+//     thiss.replaceWith(component);
+// }
+
+globalThis.vanRegister = function (id, component) {
+    globalThis.VANCOMPONENTS[id] = component;
+}
+
+var settingsIcon = new FAIcon({
+    name: "fa-gear"
+});
+var settings = new PrimaryButton({
+    onClick: () => {
+        USER_INTERFACE.AdvancedMenu.openMenu(APPLICATION_CONTEXT.settingsMenuId);
+    },
+    id: "settingsButton",
+}, settingsIcon);
+
+settings.set(PrimaryButton.SIZE.SMALL)
+
+vanRegister("settings", settings);
+
+var tutorialIcon = new FAIcon({
+    name: "fa-graduation-cap"
+});
+
+var tutorial = new PrimaryButton({
+    onClick: () => {
+        USER_INTERFACE.Tutorials.show();
+    },
+    id: "tutorialButton",
+}, tutorialIcon, "tutorial");
+tutorial.set(PrimaryButton.SIZE.SMALL)
+vanRegister("tutorial", tutorial);
+
+var pluginsIcon = new FAIcon({
+    name: "fa-puzzle-piece"
+});
+var plugins = new PrimaryButton({
+    onClick: () => {
+        USER_INTERFACE.AdvancedMenu.openMenu(APPLICATION_CONTEXT.pluginsMenuId);
+    },
+    id: "pluginsButton",
+}, pluginsIcon, "plugins");
+plugins.set(PrimaryButton.SIZE.SMALL)
+vanRegister("plugins", plugins);
+
+globalThis.window.addEventListener("load", function () {
+    for (const id in globalThis.VANCOMPONENTS) {
+        const component = globalThis.VANCOMPONENTS[id];
+        let elements = document.querySelectorAll(`[data-van="${id}"]`);
+        elements.forEach(element => {
+            console.log(element);
+            component.attachTo(element);
+        });
+    }
+
+});
