de331d201bf3aa2e635e9da1832115c2ac8c4acb
Jiří Buchta <jiribuchta02@gmail.com>
2025-02-24 13:30:53 +0100

    changed menu class for better workflow

--- ui/components/menu.mjs
+++ ui/components/menu.mjs
@@ -27,16 +27,17 @@ class Menu extends BaseComponent {
      * @param  {...any} args 
      */
     constructor(options, ...args) {
-        super(options, ...args);
-
+        super(options,);
         this.tabs = args[0];
-        this.buttonGroup = new ui.Join({ id: this.hash + "buttonGroup", style: ui.Join.STYLE.HORIZONTAL },);
-        this.contentDiv = new ui.Div({ id: this.hash + "content" },);;
-        this.mainDiv = new ui.Div({ id: this.hash + "main", base: "flex gap-1 bg-base-200", flex: "flex-col" }, this.buttonGroup, this.contentDiv);
+
+        this.header = new ui.Join({ id: this.hash + "header", style: ui.Join.STYLE.HORIZONTAL },);
+        this.body = new ui.Div({ id: this.hash + "body" },);
+
 
         this.classMap["base"] = "flex gap-1 bg-base-200";
         this.classMap["orientation"] = Menu.ORIENTATION.TOP;
         this.classMap["buttonSide"] = Menu.BUTTONSIDE.LEFT;
+        this.classMap["flex"] = "flex-col";
         if (options) {
             this._applyOptions(options, "orientation", "buttonSide");
         }
@@ -44,13 +45,13 @@ class Menu extends BaseComponent {
 
 
     create() {
+        var buttons = [];
         for (const [t, _] of Object.entries(this.tabs)) {
             new ui.Button({
-                join: "join-item",
                 onClick: () => {
                     var was_visible = document.getElementById(this.hash + "c-" + t).style.display === "block";
-                    var content_divs = document.getElementById(this.hash + "content").childNodes;
-                    for (var c_d of content_divs) {
+                    var body_divs = document.getElementById(this.hash + "body").childNodes;
+                    for (var c_d of body_divs) {
                         c_d.style.display = "none";
                     }
                     if (!was_visible) {
@@ -58,13 +59,16 @@ class Menu extends BaseComponent {
                     }
                 },
                 id: this.hash + "b-" + t,
-            }, t).attachTo(this.buttonGroup);
+            }, t).attachTo(this.header);
 
-            new ui.Div({ id: this.hash + "c-" + t, display: "display-none" }, ...this.tabs[t]).attachTo(this.contentDiv);
+            new ui.Div({ id: this.hash + "c-" + t, display: "display-none" }, ...this.tabs[t]).attachTo(this.body);
         }
-
-        this.mainDiv.refreshState(); // TODO why is this needed?????? -> its not children of this component, that is why its not called in the base class
-        return this.mainDiv.create();
+        this.header.attachTo(this);
+        this.body.attachTo(this);
+        return div(
+            { ...this.commonProperties },
+            ...this.children
+        );
     }
 
     generateCode() {
@@ -73,15 +77,15 @@ class Menu extends BaseComponent {
 }
 
 Menu.ORIENTATION = {
-    TOP: function () { this.mainDiv.setClass("flex", "flex-col"); this.buttonGroup.set(ui.Join.STYLE.HORIZONTAL); },
-    BOTTOM: function () { this.mainDiv.setClass("flex", "flex-col-reverse"); this.buttonGroup.set(ui.Join.STYLE.HORIZONTAL); },
-    LEFT: function () { this.mainDiv.setClass("flex", "flex-row"); this.buttonGroup.set(ui.Join.STYLE.VERTICAL); },
-    RIGHT: function () { this.mainDiv.setClass("flex", "flex-row-reverse"); this.buttonGroup.set(ui.Join.STYLE.VERTICAL); }
+    TOP: function () { this.setClass("flex", "flex-col"); this.header.set(ui.Join.STYLE.HORIZONTAL); },
+    BOTTOM: function () { this.setClass("flex", "flex-col-reverse"); this.header.set(ui.Join.STYLE.HORIZONTAL); },
+    LEFT: function () { this.setClass("flex", "flex-row"); this.header.set(ui.Join.STYLE.VERTICAL); },
+    RIGHT: function () { this.setClass("flex", "flex-row-reverse"); this.header.set(ui.Join.STYLE.VERTICAL); }
 }
 
 Menu.BUTTONSIDE = {
-    LEFT: function () { this.buttonGroup.setClass("flex", ""); },
-    RIGHT: function () { this.buttonGroup.setClass("flex", "flex-end"); },
+    LEFT: function () { this.header.setClass("flex", ""); },
+    RIGHT: function () { this.header.setClass("flex", "flex-end"); },
 }
 
 export { Menu };
