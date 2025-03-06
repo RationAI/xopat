984936ba4255cc2abd5815fa849ab7ad53dfa3a4
Jiří Buchta <jiribuchta02@gmail.com>
2025-03-06 12:31:39 +0100

    working on adding new UI menu to xopat, added TODO notes

--- src/app.js
+++ src/app.js
@@ -62,6 +62,7 @@ function initXopat(PLUGINS, MODULES, ENV, POST_DATA, PLUGINS_FOLDER, MODULES_FOL
         I18NCONFIG = savedState.I18NCONFIG;
     }
 
+    initMenu();
     initXopatUI();
 
     //Setup language and parse config if function provided
