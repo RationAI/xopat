9baf09286037558f7f20fbc4f003203c277b815d
Jiří Buchta <jiribuchta02@gmail.com>
2025-07-23 09:59:54 +0200

    removed unused components: collapse, menuButton, menuDropdown

--- ui/index.mjs
+++ ui/index.mjs
@@ -4,20 +4,17 @@ globalThis.VANCOMPONENTS = {};
 import van from "./vanjs.mjs";
 
 import { Button } from "./components/buttons.mjs";
-import { Collapse } from "./components/collapse.mjs";
 import { FAIcon } from "./components/fa-icon.mjs";
 import { Join } from "./components/join.mjs";
 import { Menu } from "./components/menu.mjs";
 import { Div } from "./components/div.mjs";
 import { MainPanel } from "./components/mainPanel.mjs";
-import { menuDropdown } from "./components/menuDropdown.mjs";
-import { MenuButton } from "./components/menuButton.mjs";
 import { MultiPanelMenuTab } from "./components/multiPanelMenuTab.mjs";
 import { MultiPanelMenu } from "./components/multiPanelMenu.mjs";
 import { FullscreenMenu } from "./components/fullscreenMenu.mjs";
 import { TabsMenu } from "./components/tabsMenu.mjs";
 
-const UI = { Button, Collapse, FAIcon, Join, Menu, Div, MainPanel, menuDropdown, MenuButton, MultiPanelMenuTab, MultiPanelMenu, FullscreenMenu, TabsMenu };
+const UI = { Button, FAIcon, Join, Menu, Div, MainPanel, MultiPanelMenuTab, MultiPanelMenu, FullscreenMenu, TabsMenu };
 globalThis.UI = UI;
 
 globalThis.vanRegister = function (id, component) {
