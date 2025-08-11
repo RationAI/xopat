d3cbe156c80c12759c4e5d263ef23a00d031c5fc
ShadowAya <jonhlad@gmail.com>
2025-08-11 20:58:15 +0200

    fix: compilation of all tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./ui/**/*"],
+  // WARNING: compiling styles for the entire app can take a few minutes
+  content: [
+    "./ui/**/*.{html,js,mjs}",
+    "./modules/**/*.{html,js}",
+    "./plugins/**/*.{html,js}",
+    "./src/**/*.{html,js}",
+    "./index.html",
+    "!**/*.min.js",
+  ],
   darkMode: ["selector", '[data-theme="catppuccin-mocha"]', '[data-theme="blood-moon"]'],
   theme: {
 
   },
-  // safelist: [
-  //   {
-  //     pattern: /./, // the "." means "everything"
-  //   },
-  // ],
-  safelist: [
-    {
-      pattern: /^opacity-(0|25|50|75|100)/,
-    },
-  ],
   plugins: [
     require("@catppuccin/tailwindcss"),
     require("daisyui")
